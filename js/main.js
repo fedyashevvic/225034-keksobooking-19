d57e216067569972506f63cae33fd827be701409
fedyashevvic <fedyashevvic@gmail.com>
2020-01-30 20:38:04 +0300

    Доработка скрпитка генерации объекта

--- js/main.js
+++ js/main.js
@@ -9,7 +9,6 @@ var rooms = [1, 2, 3, 4, 5];
 var checkInOutTimes = ['12:00', '13:00', '14:00'];
 var features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var photos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
-var yPositions = [130, 630];
 var mapWidth = document.querySelector('.map').clientWidth;
 var pinWidth = 25;
 var pinHeight = 70;
@@ -19,39 +18,45 @@ var bookingData = [];
 var similarBlock = document.querySelector('#pin').content.querySelector('.map__pin');
 var fragment = document.createDocumentFragment();
 
-var generateBookingData = function () {
-  var getRamdomElement = function (arr) {
-    var elNum = Math.round(Math.random() * (arr.length - 1));
-    return arr[elNum];
+var getRamdomElement = function (arr) {
+  var elNum = Math.round(Math.random() * (arr.length - 1));
+  return arr[elNum];
+};
+var getRandomUniqueElement = function (arr) {
+  var elNum = Math.round(Math.random() * (arr.length - 1));
+  var currEl = arr[elNum];
+  arr.splice(elNum, 1);
+  return currEl;
+};
+var getRandomArray = function (arr) {
+  var oldArr = arr.slice().sort();
+  var newArr = [];
+  var numberOfFeatures = Math.round(Math.random() * (oldArr.length - 1));
+  for (var j = 0; j <= numberOfFeatures; j++) {
+    var currentIndex = Math.round(Math.random() * (oldArr.length - 1));
+    newArr.push(oldArr[currentIndex]);
+    oldArr.splice(currentIndex, 1);
+  }
+  return newArr;
+};
+var getLocationObj = function (yFirstPosition, ySecondPositions) {
+  var location = {
+    'x': Math.round(Math.random() * mapWidth - pinWidth),
+    'y': Math.round((Math.random() * (ySecondPositions - yFirstPosition) + yFirstPosition) - pinHeight),
   };
-  var getRandomUniqueElement = function (arr) {
-    var elNum = Math.round(Math.random() * (arr.length - 1));
-    var currEl = arr[elNum];
-    arr.splice(elNum, 1);
-    return currEl;
-  };
-  var getRandomArray = function (arr) {
-    var oldArr = arr.slice().sort();
-    var newArr = [];
-    var numberOfFeatures = Math.round(Math.random() * (oldArr.length - 1));
-    for (var j = 0; j <= numberOfFeatures; j++) {
-      var currentIndex = Math.round(Math.random() * (oldArr.length - 1));
-      newArr.push(oldArr[currentIndex]);
-      oldArr.splice(currentIndex, 1);
-    }
-    return newArr;
-  };
+  return location;
+};
 
+var generateBookingData = function () {
   for (var i = 0; i < numberOfData; i++) {
-    var x = Math.round(Math.random() * mapWidth);
-    var y = Math.round(Math.random() * (yPositions[1] - yPositions[0]) + yPositions[0]);
+    var location = getLocationObj(130, 630);
     var currentData = {
       author: {
         avatar: 'img/avatars/user' + getRandomUniqueElement(avatars) + '.png',
       },
       offer: {
         title: getRandomUniqueElement(titles),
-        address: x + ', ' + y,
+        address: location.x + ', ' + location.y,
         price: getRamdomElement(prices),
         type: getRamdomElement(types),
         rooms: getRamdomElement(rooms),
@@ -62,10 +67,7 @@ var generateBookingData = function () {
         description: getRamdomElement(titles),
         photos: getRandomArray(photos),
       },
-      location: {
-        x: x,
-        y: y,
-      }
+      location: location,
     };
     bookingData.push(currentData);
   }
@@ -74,7 +76,7 @@ var generateBookingData = function () {
 generateBookingData();
 var renderPinElement = function (data) {
   var pinElement = similarBlock.cloneNode(true);
-  var positioning = 'left: ' + (data.location.x - pinWidth) + 'px; top: ' + (data.location.y - pinHeight) + 'px;';
+  var positioning = 'left: ' + data.location.x + 'px; top: ' + data.location.y + 'px;';
   pinElement.setAttribute('style', positioning);
   pinElement.querySelector('img').setAttribute('src', data.author.avatar);
   pinElement.querySelector('img').setAttribute('alt', data.offer.title);
