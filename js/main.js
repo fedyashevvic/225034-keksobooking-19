31685abaa791aa7aeb743db58fc9904690e9215f
fedyashevvic <fedyashevvic@gmail.com>
2020-02-11 12:40:04 +0300

    Добавлены скрипты активации страницы и валидации кол-ва комнат и гостей

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var ACTIVE_PIN_HEIGHT = 84;
 var numberOfData = 8;
 var avatars = ['01', '02', '03', '04', '05', '06', '07', '08'];
 var titles = ['Лучшая квартира в городе', 'Новая квартира', 'Прямо в центре города', 'Пентхаус в небоскребе', 'Двухкомнатная квартира после ремонта', 'Студия в новостройке', 'Большая квартира у метро', 'Квартира для кодеров'];
@@ -16,8 +17,17 @@ var pinHeight = 70;
 var bookingData = [];
 
 var similarBlock = document.querySelector('#pin').content.querySelector('.map__pin');
-var similarPinDetailsBlock = document.querySelector('#card').content.querySelector('.map__card');
+// var similarPinDetailsBlock = document.querySelector('#card').content.querySelector('.map__card');
 var fragment = document.createDocumentFragment();
+
+var formInputs = document.querySelectorAll('.ad-form fieldset');
+var filtersFormInputes = document.querySelectorAll('.map__filters input');
+var filtersFormSelects = document.querySelectorAll('.map__filters select');
+var mainPin = document.querySelector('.map__pin--main');
+var mainPinWidth = (mainPin.clientWidth / 2);
+var mainPinHeight = (mainPin.clientHeight / 2);
+var roomSelect = document.querySelector('#room_number');
+var capasitySelect = document.querySelector('#capacity');
 
 var getRamdomElement = function (arr) {
   var elNum = Math.round(Math.random() * (arr.length - 1));
@@ -86,112 +96,174 @@ var renderPinElement = function (data) {
   fragment.appendChild(pinElement);
 };
 
+
 var renderPinItem = function (data) {
   for (var i = 0; i < numberOfData; i++) {
     renderPinElement(data[i]);
   }
   document.querySelector('.map__pins').appendChild(fragment);
-  document.querySelector('.map').classList.remove('map--faded');
-};
-
-var hideElement = function (el) {
-  el.style.display = 'none';
-};
-
-var setNewData = function (data, key, addText) {
-  if (data) {
-    key.textContent = addText;
-  } else {
-    hideElement(key);
-  }
-};
-
-var defineApartnemtType = function (data, key) {
-  switch (data) {
-    case 'flat':
-      key.textContent = 'Квартира';
-      break;
-    case 'bungalo':
-      key.textContent = 'Бунгало';
-      break;
-    case 'house':
-      key.textContent = 'Дом';
-      break;
-    case 'palace':
-      key.textContent = 'Дворец';
-      break;
-    default:
-      key.style.display = 'none';
-  }
-};
-
-var setPhotos = function (data, key, parent) {
-  if (data) {
-    key.querySelector('img').setAttribute('src', data[0]);
-    for (var i = 1; i < data.length; i++) {
-      var photo = parent.querySelector('.popup__photos').cloneNode(true);
-      photo.querySelector('img').setAttribute('src', data[i]);
-      parent.appendChild(photo);
-    }
-  } else {
-    hideElement(key);
-  }
-};
-
-var setFeatures = function (data, key, parent) {
-  for (var j = 0; j < key.length; j++) {
-    key[j].remove();
-  }
-  if (data) {
-    for (var k = 0; k < data.length; k++) {
-      var newFeature = document.createElement('li');
-      var currentClass = 'popup__feature--' + data[k];
-      newFeature.classList.add('popup__feature', currentClass);
-      parent.querySelector('.popup__features').appendChild(newFeature);
-    }
-  }
-};
-
-var setAvatar = function (data, key) {
-  if (data) {
-    key.setAttribute('src', data);
-  } else {
-    hideElement(key);
-  }
-};
-
-var renderPinDetails = function (data, key) {
-  var pinDetailsElement = similarPinDetailsBlock.cloneNode(true);
-  var title = pinDetailsElement.querySelector('.popup__title');
-  var address = pinDetailsElement.querySelector('.popup__text--address');
-  var price = pinDetailsElement.querySelector('.popup__text--price');
-  var type = pinDetailsElement.querySelector('.popup__type');
-  var houseCapasity = pinDetailsElement.querySelector('.popup__text--capacity');
-  var checkInTime = pinDetailsElement.querySelector('.popup__text--time');
-  var featuresItems = pinDetailsElement.querySelector('.popup__features').querySelectorAll('li');
-  var description = pinDetailsElement.querySelector('.popup__description');
-  var apartPhotos = pinDetailsElement.querySelector('.popup__photos');
-  var avatar = pinDetailsElement.querySelector('.popup__avatar');
-  var priceTextContent = data[key].offer.price + '₽/ночь';
-  var checkInTimeTextContent = 'Заезд после ' + data[key].offer.checkin + ', выезд до ' + data[key].offer.checkout + '.';
-  var houseCapasityTextContent = data[key].offer.rooms + ' комнаты для ' + data[key].offer.guests + ' гостей.';
-
-  setNewData(data[key].offer.title, title, data[key].offer.title);
-  setNewData(data[key].offer.address, address, data[key].offer.address);
-  setNewData(data[key].offer.price, price, priceTextContent);
-  defineApartnemtType(data[key].offer.type, type);
-  setNewData(data[key].offer.rooms, houseCapasity);
-  setNewData(data[key].offer.guests, houseCapasity, houseCapasityTextContent);
-  setNewData(data[key].offer.checkin, checkInTime, checkInTimeTextContent);
-  setFeatures(data[key].offer.features, featuresItems, pinDetailsElement);
-  setNewData(data[key].offer.description, description, data[key].offer.description);
-  setPhotos(data[key].offer.photos, apartPhotos, pinDetailsElement);
-  setAvatar(data[key].author.avatar, avatar);
-
-  document.querySelector('.map__filters-container').before(pinDetailsElement);
 };
 
 generateBookingData();
-renderPinItem(bookingData);
-renderPinDetails(bookingData, 0);
+
+
+var mainPinPosition = function (x, y) {
+  var currentPinPosition = Math.round((mainPin.offsetLeft + (x))) + '; ' + Math.round((mainPin.offsetTop + (y)));
+  document.querySelector('input#address').value = currentPinPosition;
+};
+
+
+var changeInputsCondition = function (collection, boolean) {
+  for (var i = 0; i < collection.length; i++) {
+    collection[i].disabled = boolean;
+  }
+};
+
+
+var disablePage = function () {
+  changeInputsCondition(formInputs, true);
+  changeInputsCondition(filtersFormInputes, true);
+  changeInputsCondition(filtersFormSelects, true);
+  mainPinPosition(mainPinWidth, mainPinHeight);
+};
+disablePage();
+
+var activatePage = function () {
+  renderPinItem(bookingData);
+  mainPinPosition(mainPinWidth, ACTIVE_PIN_HEIGHT);
+  changeInputsCondition(formInputs, false);
+  changeInputsCondition(filtersFormInputes, false);
+  changeInputsCondition(filtersFormSelects, false);
+  document.querySelector('.map').classList.remove('map--faded');
+  document.querySelector('.ad-form').classList.remove('ad-form--disabled');
+
+};
+
+mainPin.addEventListener('mousedown', function (evt) {
+  if (!evt.button) {
+    activatePage();
+  }
+});
+mainPin.addEventListener('keydown', function (evt) {
+  if (evt.key === 'Enter') {
+    activatePage();
+  }
+});
+
+var submitCheckSelect = function () {
+  if (roomSelect.value === '1' && capasitySelect.value !== '1') {
+    capasitySelect.setCustomValidity('При данном кол-ве комнат поместится только 1 человек');
+  } else if (roomSelect.value === '2' && (capasitySelect.value === '3' || capasitySelect.value === '0')) {
+    capasitySelect.setCustomValidity('При данном кол-ве комнат поместится до 2х человек');
+  } else if (roomSelect.value === '3' && capasitySelect.value === '0') {
+    capasitySelect.setCustomValidity('При данном кол-ве комнат поместится до 3х человек');
+  } else if (roomSelect.value === '100' && capasitySelect.value !== '0') {
+    capasitySelect.setCustomValidity('При данном кол-ве комнат, допустим только вариант "Не для гостей"');
+  } else {
+    capasitySelect.setCustomValidity('');
+  }
+};
+
+capasitySelect.addEventListener('change', submitCheckSelect);
+roomSelect.addEventListener('change', submitCheckSelect);
+
+
+// var hideElement = function (el) {
+//   el.style.display = 'none';
+// };
+
+// var setNewData = function (data, key, addText) {
+//   if (data) {
+//     key.textContent = addText;
+//   } else {
+//     hideElement(key);
+//   }
+// };
+
+// var defineApartnemtType = function (data, key) {
+//   switch (data) {
+//     case 'flat':
+//       key.textContent = 'Квартира';
+//       break;
+//     case 'bungalo':
+//       key.textContent = 'Бунгало';
+//       break;
+//     case 'house':
+//       key.textContent = 'Дом';
+//       break;
+//     case 'palace':
+//       key.textContent = 'Дворец';
+//       break;
+//     default:
+//       key.style.display = 'none';
+//   }
+// };
+
+// var setPhotos = function (data, key, parent) {
+//   if (data) {
+//     key.querySelector('img').setAttribute('src', data[0]);
+//     for (var i = 1; i < data.length; i++) {
+//       var photo = parent.querySelector('.popup__photos').cloneNode(true);
+//       photo.querySelector('img').setAttribute('src', data[i]);
+//       parent.appendChild(photo);
+//     }
+//   } else {
+//     hideElement(key);
+//   }
+// };
+
+// var setFeatures = function (data, key, parent) {
+//   for (var j = 0; j < key.length; j++) {
+//     key[j].remove();
+//   }
+//   if (data) {
+//     for (var k = 0; k < data.length; k++) {
+//       var newFeature = document.createElement('li');
+//       var currentClass = 'popup__feature--' + data[k];
+//       newFeature.classList.add('popup__feature', currentClass);
+//       parent.querySelector('.popup__features').appendChild(newFeature);
+//     }
+//   }
+// };
+
+// var setAvatar = function (data, key) {
+//   if (data) {
+//     key.setAttribute('src', data);
+//   } else {
+//     hideElement(key);
+//   }
+// };
+
+// var renderPinDetails = function (data, key) {
+//   var pinDetailsElement = similarPinDetailsBlock.cloneNode(true);
+//   var title = pinDetailsElement.querySelector('.popup__title');
+//   var address = pinDetailsElement.querySelector('.popup__text--address');
+//   var price = pinDetailsElement.querySelector('.popup__text--price');
+//   var type = pinDetailsElement.querySelector('.popup__type');
+//   var houseCapasity = pinDetailsElement.querySelector('.popup__text--capacity');
+//   var checkInTime = pinDetailsElement.querySelector('.popup__text--time');
+//   var featuresItems = pinDetailsElement.querySelector('.popup__features').querySelectorAll('li');
+//   var description = pinDetailsElement.querySelector('.popup__description');
+//   var apartPhotos = pinDetailsElement.querySelector('.popup__photos');
+//   var avatar = pinDetailsElement.querySelector('.popup__avatar');
+//   var priceTextContent = data[key].offer.price + '₽/ночь';
+//   var checkInTimeTextContent = 'Заезд после ' + data[key].offer.checkin + ', выезд до ' + data[key].offer.checkout + '.';
+//   var houseCapasityTextContent = data[key].offer.rooms + ' комнаты для ' + data[key].offer.guests + ' гостей.';
+
+//   setNewData(data[key].offer.title, title, data[key].offer.title);
+//   setNewData(data[key].offer.address, address, data[key].offer.address);
+//   setNewData(data[key].offer.price, price, priceTextContent);
+//   defineApartnemtType(data[key].offer.type, type);
+//   setNewData(data[key].offer.rooms, houseCapasity);
+//   setNewData(data[key].offer.guests, houseCapasity, houseCapasityTextContent);
+//   setNewData(data[key].offer.checkin, checkInTime, checkInTimeTextContent);
+//   setFeatures(data[key].offer.features, featuresItems, pinDetailsElement);
+//   setNewData(data[key].offer.description, description, data[key].offer.description);
+//   setPhotos(data[key].offer.photos, apartPhotos, pinDetailsElement);
+//   setAvatar(data[key].author.avatar, avatar);
+
+//   document.querySelector('.map__filters-container').before(pinDetailsElement);
+// };
+
+// renderPinDetails(bookingData, 0);
 
